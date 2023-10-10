14eaf732c59b90ff085d6af2abd2d5c4d8af4bfc
mca <aswinko479@gmail.com>
2023-10-10 11:44:02 +0530

    program 15 updated

--- Cycle-2/swap_the_char.py
+++ Cycle-2/swap_the_char.py
@@ -1,4 +1,6 @@
 a = input("Enter the string 1 : ")
 b = input("Enter the string 2 : ")
 
-print(a.replace(a[0], b[0]), b.replace(b[0], a[0]))
+#print(a.replace(a[0], b[0]), b.replace(b[0], a[0]))
+
+print(b[0]+a[1:] + " " + a[0]+b[1:])
